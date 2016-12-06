2610c3560a2bf969b5dba3403d17bafda7d592d9
sali mali <sali mali>
2016-12-06 10:08:27 -0500

    update email section adding a condition

--- sendemail.php
+++ sendemail.php
@@ -4,8 +4,16 @@
     $subject = "PHP Mail Test script";
 	$message = "This is a test to check the PHP Mail functionality";
     $headers = "From:";
-    mail($to,$subject,$message, $headers);
-    echo "Test email sent";	
+    if (mail($to,$subject,$message, $headers);)
+	{
+		    echo "Test email sent";	
+
+	}
+	else
+	{
+		    echo "ERROR";	
+
+	}
 
  
   
