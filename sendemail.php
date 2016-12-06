cf0e40d1c0ffaf8313eb7a444ad6cdd40ae251cd
sali mali <sali mali>
2016-12-06 10:28:38 -0500

    update mgs section in sendingemail

--- sendemail.php
+++ sendemail.php
@@ -2,10 +2,8 @@
 
     $to      = 'dscodetest@mailinator.com'; 
 $subject = 'the subject'; 
-$message = 'hello'; 
-$headers = 'From: test@mailinator.com' . "\r\n" . 
-           'Reply-To: test@mailinator.com' . "\r\n" . 
-           'X-Mailer: PHP/' . phpversion(); 
+$message = <<<EMAIL HELLO EMAIL; 
+$headers = 'test@mailinator.com';
 
 if(mail($to, $subject, $message, $headers)){ 
     echo "Mail Sent"; 
