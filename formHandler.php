b40b82241326f2879444b1c18b751f9285987118
sali mali <sali mali>
2016-12-06 15:11:44 -0500

    update sendemail page

--- formHandler.php
+++ formHandler.php
@@ -1,18 +1,19 @@
 
 <?php
-
-if(isset($_POST['submit']))
-{
-	$field_first_name=$_POST['field_first_name'];
-	$field_birthdate=$_POST['field_birthdate'];
-	$mail=$_POST['mail'];
-	$field_mobile=$_POST['field_mobile'];
-	$pass=$_POST['pass'];
 	
 require('vendor/autoload.php');
 define('AMQP_DEBUG', true);
 use PhpAmqpLib\Connection\AMQPConnection;
 use PhpAmqpLib\Message\AMQPMessage;
+
+if(isset($_POST['submit']))
+{
+	//$field_first_name=$_POST['field_first_name'];
+	//$field_birthdate=$_POST['field_birthdate'];
+	$mail=$_POST['mail'];
+	//$field_mobile=$_POST['field_mobile'];
+	//$pass=$_POST['pass'];
+
 $url = parse_url(getenv('CLOUDAMQP_URL'));
 $conn = new AMQPConnection($url['host'], 5672, $url['user'], $url['pass'], substr($url['path'], 1));
 $ch = $conn->channel();
@@ -23,7 +24,7 @@ $ch->queue_declare($queue, false, true, false, false);
 $ch->exchange_declare($exchange, 'direct', true, true, false);
 $ch->queue_bind($queue, $exchange);
 
-$msg_body = $field_first_name." " .$field_birthdate."".$mail." " .$field_mobile." ".$pass;
+$msg_body = $mail;
 $msg = new AMQPMessage($msg_body, array('content_type' => 'text/plain', 'delivery_mode' => 2));
 $ch->basic_publish($msg, $exchange);
 
