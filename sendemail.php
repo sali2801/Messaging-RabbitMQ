c7892151336419f5cde7e37f183efc99bb171690
sali mali <sali mali>
2016-12-06 11:38:56 -0500

    update sendemail page

--- sendemail.php
+++ sendemail.php
@@ -7,12 +7,16 @@ require 'vendor/autoload.php';
 $sendgrid = new SendGrid("SENDGRID_APIKEY");
 $email    = new SendGrid\Email();
 
-$email->addTo("salihamallem92@gmail.com")
-      ->setFrom("you@youremail.com")
+$email->addTo("dscodetest@mailinator.com  ")
+      ->setFrom("as_mallem@esi.dz")
       ->setSubject("Sending with SendGrid is Fun")
       ->setHtml("and easy to do anywhere, even with PHP");
 
 $sendgrid->send($email);
+
+    echo $email;	
+
+
       ?>
  
   
