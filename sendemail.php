52e3c339358fbc6a0b57422cca07edf9843d86b8
sali mali <sali mali>
2016-12-06 11:15:40 -0500

    update sendemail page

--- sendemail.php
+++ sendemail.php
@@ -1,15 +1,26 @@
 <?php
 
 
-$message = 'Hilklllllllllllllllllllllll'; 
-
-
-if(mail('dscodetest@mailinator.com', 'test@mailinator.com', $message))
-{ 
-    echo "Mail Sent"; 
-}else{ 
-    echo "***ERROR***"; 
-} 
+ <?php
+         $to = "dscodetest@mailinator.com";
+         $subject = "This is subject";
+         
+         $message = "<b>This is HTML message.</b>";
+         $message .= "<h1>This is headline.</h1>";
+         
+         $header = "From:abc@somedomain.com \r\n";
+         $header .= "Cc:afgh@somedomain.com \r\n";
+         $header .= "MIME-Version: 1.0\r\n";
+         $header .= "Content-type: text/html\r\n";
+         
+         $retval = mail ($to,$subject,$message,$header);
+         
+         if( $retval == true ) {
+            echo "Message sent successfully...";
+         }else {
+            echo "Message could not be sent...";
+         }
+      ?>
  
   
  ?>
