509c08d2ac9bec0e1cf2740b354d2e5d20bb4024
sali mali <sali mali>
2016-12-06 18:48:29 -0500

    emailService

--- bin/emailService.php
+++ bin/emailService.php
@@ -11,6 +11,20 @@ $url = parse_url(getenv('CLOUDAMQP_URL'));
 $conn = new AMQPConnection($url['host'], 5672, $url['user'], $url['pass'], substr($url['path'], 1));
 $ch = $conn->channel();
 $queue = 'basic_get_queue';
+$ch->queue_declare($queue, false, true, false, false);
+echo ' [*] Waiting for messages. To exit press CTRL+C', "\n";
+
+$callback = function($msg) {
+  echo " [x] Received ", $msg->body, "\n";
+};
+
+$ch->basic_consume($queue, '', false, true, false, false, $callback);
+
+while(count($ch->callbacks)) {
+    $ch->wait();
+}
+
+/* 
 
 $callback = function($msg) {
     $retrived_msg = $ch->basic_get($queue);
@@ -25,7 +39,7 @@ $callback = function($msg) {
 while(count($ch->callbacks)) {
     $cha->wait();
 }
-
+ */
 
 $ch->close();
 $conn->close();
