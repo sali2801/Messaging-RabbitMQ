589f109ec21406561f6b4f08b901a6b1cb1c1b15
sali mali <sali mali>
2016-12-06 15:01:57 -0500

    remove semicolumn

--- thankYou.php
+++ thankYou.php
@@ -8,43 +8,12 @@ if(isset($_POST))
 	$field_mobile=$_POST['field_mobile'];
 	$pass=$_POST['pass'];
 
-require('vendor/autoload.php');
-define('AMQP_DEBUG', true);
-use PhpAmqpLib\Connection\AMQPConnection;
-use PhpAmqpLib\Message\AMQPMessage;
-$url = parse_url(getenv('CLOUDAMQP_URL'));
-$conn = new AMQPConnection($url['host'], 5672, $url['user'], $url['pass'], substr($url['path'], 1));
-$ch = $conn->channel();
+	echo $mail;
 
-$exchange = 'amq.direct';
-$queue = 'basic_get_queue';
-$ch->queue_declare($queue, false, true, false, false);
-$ch->exchange_declare($exchange, 'direct', true, true, false);
-$ch->queue_bind($queue, $exchange);
-
-$msg_body = $mail;
-$msg = new AMQPMessage($msg_body, array('content_type' => 'text/plain', 'delivery_mode' => 2));
-$ch->basic_publish($msg, $exchange);
-
-$retrived_msg = $ch->basic_get($queue);
-$mymsg=$retrived_msg->body;
-//var_dump($retrived_msg->body);
-	echo "<p bgcolor=\"red\"> Thank you ;)". $mymsg ."</p>";
-
-
-	
-	
-$ch->basic_ack($retrived_msg->delivery_info['delivery_tag']);
-
-
-
-
-$ch->close();
-$conn->close();
 }
 else
 {
-	echo'hhhhhhhhhhhhhhhhhhhi';
+	echo 'hhhhhhhhhhhhhhhhhhhi';
 }
   
  ?>
