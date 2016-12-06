3cd0f9c039644dae103ede8381bd10280180ea9d
sali mali <sali mali>
2016-12-06 10:32:43 -0500

    updating receiver address email

--- sendemail.php
+++ sendemail.php
@@ -1,9 +1,12 @@
 <?php
 
-    $to      = 'dscodetest@mailinator.com'; 
-$subject = 'the subject'; 
-$message = <<<EMAIL HELLO EMAIL; 
-$headers = 'test@mailinator.com';
+    $to      = "dscodetest@mailinator.com"; 
+$subject = "the subject"; 
+$message = <<<EMAIL 
+HELLO
+
+ EMAIL; 
+$headers = "From:test@mailinator.com";
 
 if(mail($to, $subject, $message, $headers)){ 
     echo "Mail Sent"; 
