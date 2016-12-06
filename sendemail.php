1ab6437cce0c3c080fb9f168fba259c1c2311432
sali mali <sali mali>
2016-12-06 13:56:07 -0500

    update sendemail page

--- sendemail.php
+++ sendemail.php
@@ -5,14 +5,15 @@ require 'vendor/autoload.php';
 // If you are not using Composer (recommended)
 // require("path/to/sendgrid-php/sendgrid-php.php");
 
-$from = new SendGrid\Email(null, "dscodetest@mailinator.com");
+$from = new SendGrid\Email(null, "test@example.com");
 $subject = "Hello World from the SendGrid PHP Library!";
-$to = new SendGrid\Email(null, "as_mallem@esi.dz");
+$to = new SendGrid\Email(null, "dscodetest@mailinator.com");
 $content = new SendGrid\Content("text/plain", "Hello, Email!");
 $mail = new SendGrid\Mail($from, $subject, $to, $content);
 
- $apiKey = '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';
- $sg = new \SendGrid($apiKey);
+$apiKey = getenv('SENDGRID_API_KEY');
+echo $apiKey;
+$sg = new \SendGrid($apiKey);
 
 $response = $sg->client->mail()->send()->post($mail);
 echo $response->statusCode();
